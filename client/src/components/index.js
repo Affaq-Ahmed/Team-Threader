7f35ba88db83ec53e868aa1b2e3dbf46707ee2d0
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-14 17:04:38 +0500

    Add new components to index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,13 @@
 import { default as ChannelContainer } from './ChannelContainer';
 import { default as ChannelListContainer } from './ChannelListContainer';
+import { default as ChannelSearch } from './ChannelSearch';
+import { default as TeamChannelList } from './TeamChannelList';
+import { default as TeamChannelPreview } from './TeamChannelPreview';
 
-export { ChannelContainer, ChannelListContainer };
+export {
+	ChannelContainer,
+	ChannelListContainer,
+	ChannelSearch,
+	TeamChannelList,
+	TeamChannelPreview,
+};
