81e25430b32ce6d8abf68579625f4f2ff9a47a76
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-14 14:58:53 +0500

    Add Stream Chat functionality to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+import { ChannelContainer, ChannelListContainer } from './components';
+
+import './app.css';
+
+const apiKey = 'ekea79dpx39r';
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div className='app__wrapper'>
+			<Chat client={client} theme='team light'>
+				<ChannelListContainer />
+				<ChannelContainer />
+			</Chat>
+		</div>
+	);
 };
 
 export default App;
