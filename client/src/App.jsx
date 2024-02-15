e72ed20ea2153ed51b4617578916f6993ce728e5
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-15 10:56:34 +0500

    refactor: convert app.js -> app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
-import { ChannelContainer, ChannelListContainer } from './components';
+import {
+	ChannelContainer,
+	ChannelListContainer,
+	Auth,
+} from './components';
 
 import './app.css';
 
@@ -10,7 +14,11 @@ const apiKey = 'ekea79dpx39r';
 
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 const App = () => {
+	if (!authToken) return <Auth />;
+
 	return (
 		<div className='app__wrapper'>
 			<Chat client={client} theme='team light'>
