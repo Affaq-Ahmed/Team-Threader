3237dfa31917010e36e4fe4b2a90a3194077c831
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-15 14:32:13 +0500

    Add state hooks and components for creating and editing channels

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
@@ -8,6 +8,7 @@ import {
 	Auth,
 } from './components';
 
+import 'stream-chat-react/dist/css/index.css';
 import './app.css';
 
 const cookies = new Cookies();
@@ -32,13 +33,28 @@ if (authToken) {
 }
 
 const App = () => {
+	const [createType, setCreateType] = useState('');
+	const [isCreating, setIsCreating] = useState(false);
+	const [isEditing, setIsEditing] = useState(false);
 	if (!authToken) return <Auth />;
 
 	return (
 		<div className='app__wrapper'>
 			<Chat client={client} theme='team light'>
-				<ChannelListContainer />
-				<ChannelContainer />
+				<ChannelListContainer
+					isCreating={isCreating}
+					setIsCreating={setIsCreating}
+					setCreateType={setCreateType}
+					setIsEditing={setIsEditing}
+				/>
+				<ChannelContainer
+					isCreating={isCreating}
+					setIsCreating={setIsCreating}
+					isEditing={isEditing}
+					setIsEditing={setIsEditing}
+					createType={createType}
+					setCreateType={setCreateType}
+				/>
 			</Chat>
 		</div>
 	);
